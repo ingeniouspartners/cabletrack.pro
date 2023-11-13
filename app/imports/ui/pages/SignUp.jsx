39043c8489eb9e2bf9cb997ed920b93a1195b575
Andrea Jans <57602919+awjans@users.noreply.github.com>
2023-11-12 17:15:20 -1000

    fixed email field schema
    Took 27 seconds

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -6,7 +6,6 @@ import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import { emailSchema } from '../../api/schema/Schemas';
 
 /**
  * SignUp component is similar to signin component, but we create a new user instead.
