1e515b49a28a492408748c60deeed9a929d7e983
Matthew Yamamoto <143032763+mnyamamo@users.noreply.github.com>
2023-11-13 11:04:58 -1000

    Made changes to editing cables

--- app/imports/ui/components/CableEdit.jsx
+++ app/imports/ui/components/CableEdit.jsx
@@ -2,42 +2,65 @@ import React from 'react';
 import swal from 'sweetalert';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import { Meteor } from 'meteor/meteor';
-import { useTracker } from 'meteor/react-meteor-data';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { useParams } from 'react-router';
 import { Cables } from '../../api/cable/Cables';
 import LoadingSpinner from './LoadingSpinner';
 
 const bridge = new SimpleSchema2Bridge(Cables.schema);
-// TODO Keep these files around until we confirm that the pages work.
 /* Renders the EditStuff page for editing a single document. */
-const CableEdit = () => {
-  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-  const { _id } = useParams();
-  // console.log('CableEdit', _id);
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { doc, ready } = useTracker(() => {
-    // Get access to Cable documents.
-    const subscription = Meteor.subscribe(Cables.userPublicationName);
-    // Determine if the subscription is ready
-    const rdy = subscription.ready();
-    // Get the document
-    const document = Cables.collection.findOne(_id);
-    return {
-      doc: document,
-      ready: rdy,
-    };
-  }, [_id]);
-  // console.log('EditStuff', doc, ready);
-  // On successful submit, insert the data.
-  const submit = (data) => {
+const CableEdit = (_id, doc, ready) => {
+  const submit = (data, formRef) => {
     const { description, refDrawingNo, refDrawingRev, system, building, zone, origination, termination, lengthPlanned, classification, cableType, conductors, voltageCable, voltageTest } = data;
-    Cables.collection.update(_id, { $set: { description, refDrawingNo, refDrawingRev, system, building, zone, origination, termination, lengthPlanned, classification, cableType, conductors, voltageCable, voltageTest } }, (error) => (error ?
-      swal('Error', error.message, 'error') :
-      swal('Success', 'Item updated successfully', 'success')));
+    if (_id) {
+      Cables.collection.update(_id, {
+        $set: {
+          description,
+          refDrawingNo,
+          refDrawingRev,
+          system,
+          building,
+          zone,
+          origination,
+          termination,
+          lengthPlanned,
+          classification,
+          cableType,
+          conductors,
+          voltageCable,
+          voltageTest,
+        },
+      }, (error) => (error ?
+        swal('Error', error.message, 'error') :
+        swal('Success', 'Item updated successfully', 'success')));
+    } else {
+      Cables.collection.insert(
+        {
+          description,
+          refDrawingNo,
+          refDrawingRev,
+          system,
+          building,
+          zone,
+          origination,
+          termination,
+          lengthPlanned,
+          classification,
+          cableType,
+          conductors,
+          voltageCable,
+          voltageTest,
+        },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Item added successfully', 'success');
+            formRef.reset();
+          }
+        },
+      );
+    }
   };
-
   return ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
--- app/imports/ui/pages/EditCable.jsx
+++ app/imports/ui/pages/EditCable.jsx
@@ -1,15 +1,9 @@
-import React from 'react';
 import swal from 'sweetalert';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { useParams } from 'react-router';
 import { Cables } from '../../api/cable/Cables';
-import LoadingSpinner from '../components/LoadingSpinner';
-
-const bridge = new SimpleSchema2Bridge(Cables.schema);
+import CableEdit from '../components/CableEdit';
 
 /* Renders the EditStuff page for editing a single document. */
 const EditCable = () => {
@@ -32,89 +26,12 @@ const EditCable = () => {
   // console.log('EditStuff', doc, ready);
   // On successful submit, insert the data.
   const submit = (data, formRef) => {
-    const { description, refDrawingNo, refDrawingRev, system, building, zone, origination, termination, lengthPlanned, classification, cableType, conductors, voltageCable, voltageTest } = data;
     if (_id) {
-      Cables.collection.update(_id, {
-        $set: {
-          description,
-          refDrawingNo,
-          refDrawingRev,
-          system,
-          building,
-          zone,
-          origination,
-          termination,
-          lengthPlanned,
-          classification,
-          cableType,
-          conductors,
-          voltageCable,
-          voltageTest,
-        },
-      }, (error) => (error ?
-        swal('Error', error.message, 'error') :
-        swal('Success', 'Item updated successfully', 'success')));
+      CableEdit(_id, doc, ready);
     } else {
-      Cables.collection.insert(
-        { description,
-          refDrawingNo,
-          refDrawingRev,
-          system,
-          building,
-          zone,
-          origination,
-          termination,
-          lengthPlanned,
-          classification,
-          cableType,
-          conductors,
-          voltageCable,
-          voltageTest },
-        (error) => {
-          if (error) {
-            swal('Error', error.message, 'error');
-          } else {
-            swal('Success', 'Item added successfully', 'success');
-            formRef.reset();
-          }
-        },
-      );
     }
 
   };
-
-  return ready ? (
-    <Container className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center"><h1>EditCable</h1></Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
-            <Card>
-              <Card.Body>
-                <TextField name="description" />
-                <TextField name="refDrawingNo" />
-                <TextField name="refDrawingRev" />
-                <TextField name="system" />
-                <TextField name="building" />
-                <TextField name="zone" />
-                <TextField name="origination" />
-                <TextField name="termination" />
-                <NumField name="lengthPlanned" />
-                <SelectField name="classification" />
-                <TextField name="cableType" />
-                <TextField name="conductors" />
-                <TextField name="voltageCable" />
-                <TextField name="voltageTest" />
-                <SubmitField value="Submit" />
-                <ErrorsField />
-                <HiddenField name="owner" />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-        </Col>
-      </Row>
-    </Container>
-  ) : <LoadingSpinner />;
 };
 
 export default EditCable;
