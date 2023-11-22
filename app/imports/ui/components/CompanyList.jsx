ff3cb0f98e35073a87add9bb797c0059518161b5
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2023-11-22 12:03:01 -1000

    fixed the Companylist.jsx file

--- app/imports/ui/components/CompanyList.jsx
+++ app/imports/ui/components/CompanyList.jsx
@@ -5,31 +5,29 @@ import CompanyListItem from './CompanyListItem';
 import { PropTypeCompany } from '../../api/propTypes/PropTypes';
 
 /* Renders a table containing all of the Company documents. Use <CableItem> to render each row. */
-const CompanyList = (companies) => {
-  return (
-    <Container className="py-3" fluid>
-      <Row className="justify-content-center">
-        <Col md={7}>
-          <Col className="text-center">
-            <h2>List Company</h2>
-          </Col>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Description</th>
-                <th>View</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {companies ? companies.forEach((company) => <CompanyListItem key={company._id} company={company} />) : ''}
-            </tbody>
-          </Table>
+const CompanyList = ({ companies }) => (
+  <Container className="py-3" fluid>
+    <Row className="justify-content-center">
+      <Col md={7}>
+        <Col className="text-center">
+          <h2>List Company</h2>
         </Col>
-      </Row>
-    </Container>
-  );
-};
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>View</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {companies.map((company) => (<CompanyListItem company={company} key={company.id} />))}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  </Container>
+);
 
 CompanyList.propTypes = {
   companies: PropTypes.arrayOf(PropTypeCompany).isRequired,
