ac547434bfd81549b19111222c416f4053360629
Andrea Jans <57602919+awjans@users.noreply.github.com>
2023-11-22 15:10:08 -1000

    Fixing List Companies, added icons for Add, View and Edit
    Took 39 minutes

--- app/imports/ui/components/CompanyList.jsx
+++ app/imports/ui/components/CompanyList.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FileEarmarkPlusFill } from 'react-bootstrap-icons';
 import CompanyListItem from './CompanyListItem';
 import { PropTypeCompany } from '../../api/propTypes/PropTypes';
+import { CombinePath, PathAddCompany } from '../../api/navigation/Navigation';
 
 /* Renders a table containing all of the Company documents. Use <CableItem> to render each row. */
 const CompanyList = ({ companies }) => (
-  <Container className="py-3" fluid>
-    <Row className="justify-content-center">
-      <Col md={7}>
-        <Col className="text-center">
-          <h2>List Company</h2>
-        </Col>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Description</th>
-              <th>View</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            { companies ? (companies.map((company) => (<CompanyListItem company={company} key={company.id} />))) : '' }
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
-  </Container>
+  <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th colSpan="2"><h2>List Company</h2></th>
+        <th><Link to={CombinePath(PathAddCompany, {})}><FileEarmarkPlusFill /></Link></th>
+      </tr>
+      <tr>
+        <th>Description</th>
+        <th>View</th>
+        <th>Edit</th>
+      </tr>
+    </thead>
+    <tbody>
+      { companies ? (companies.map((company) => (<CompanyListItem company={company} key={company.id} />))) : '' }
+    </tbody>
+  </Table>
 );
 
 CompanyList.propTypes = {
--- app/imports/ui/components/CompanyListItem.jsx
+++ app/imports/ui/components/CompanyListItem.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FileEarmarkFill, PencilFill } from 'react-bootstrap-icons';
 import { PropTypeCompany } from '../../api/propTypes/PropTypes';
+import { CombinePath, PathViewCompany, PathEditCompany } from '../../api/navigation/Navigation';
 
 /** Renders a single row in the List Cables table. See pages/ListCables.jsx. */
 const CompanyListItem = ({ company }) => (
   <tr>
     <td>{company.name}</td>
     <td>
-      <Link to={`/${company._id}`}>View</Link>
+      <Link to={CombinePath(PathViewCompany, company)}><FileEarmarkFill /></Link>
     </td>
     <td>
-      <Link to={`/${company._id}/edit`}>Edit</Link>
+      <Link to={CombinePath(PathEditCompany, company)}><PencilFill /></Link>
     </td>
   </tr>
 
