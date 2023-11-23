cf7edb27f875532672218c07bd2258f06b20623c
Andrea Jans <57602919+awjans@users.noreply.github.com>
2023-11-22 15:36:11 -1000

    Fixing List Companies, added icons for Add, View and Edit
    Took 34 seconds

--- app/imports/ui/components/CompanyView.jsx
+++ app/imports/ui/components/CompanyView.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { PencilFill } from 'react-bootstrap-icons';
