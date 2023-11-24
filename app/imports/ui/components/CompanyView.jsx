09f6d16fd59fbca8ebd5870157d0876b218435fb
Andrea Jans <57602919+awjans@users.noreply.github.com>
2023-11-23 17:55:34 -1000

    Fixing Company View (page and component) All of the Company functions (except Delete) work [List, View, Edit/Add]
    Took 44 minutes
    
    Took 2 minutes

--- app/imports/ui/components/CompanyView.jsx
+++ app/imports/ui/components/CompanyView.jsx
@@ -4,21 +4,29 @@ import { Link } from 'react-router-dom';
 import { PencilFill } from 'react-bootstrap-icons';
 import { PropTypeCompany } from '../../api/propTypes/PropTypes';
 import { CombinePath, PathEditCompany } from '../../api/navigation/Navigation';
+import { formatAddress } from '../../api/schema/Schemas';
 
 /* Renders a table containing one of the Cable documents. Use <CableItem> to render each row. */
 const CompanyView = ({ company }) => {
   const editPath = CombinePath(PathEditCompany, { companyID: company._id });
   return (
     <Card>
-      <Card.Header as="h5">Cable Pull In</Card.Header>
+      <Card.Header>
+        <Card.Title>View Company</Card.Title>
+      </Card.Header>
       <Card.Body>
-        <Card.Title>{company.name}</Card.Title>
-        <Card.Subtitle>{company.description}</Card.Subtitle>
-        <Card.Text>{company.notes}</Card.Text>
-        <Card.Footer>
-          <Link to={editPath}><PencilFill /></Link>
-        </Card.Footer>
+        <Card.Text>{company.name}</Card.Text>
+        <Card.Text>{formatAddress(company.address)}</Card.Text>
+        <Card.Text>{company.phone}</Card.Text>
+        <Card.Text>{company.fax}</Card.Text>
+        <Card.Text>{company.email}</Card.Text>
+        <Card.Text>{company.logoURL}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <Link to={editPath}>
+          <PencilFill />
+        </Link>
+      </Card.Footer>
     </Card>
   );
 };
--- app/imports/ui/pages/ViewCompany.jsx
+++ app/imports/ui/pages/ViewCompany.jsx
@@ -5,6 +5,8 @@ import { useParams } from 'react-router';
 import { Companies } from '../../api/company/Companies';
 import CompanyView from '../components/CompanyView';
 import LoadingSpinner from '../components/LoadingSpinner';
+import { Col, Container, Row } from 'react-bootstrap';
+import CompanyEdit from '../components/CompanyEdit';
 
 /* Please replace the guts of this page with the right code. */
 const ViewCompany = () => {
@@ -22,7 +24,15 @@ const ViewCompany = () => {
       ready: rdy,
     };
   }, [companyID]);
-  return (ready ? <CompanyView key={doc._id} company={doc} /> : <LoadingSpinner />);
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <CompanyView company={doc} />
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
 };
 
 export default ViewCompany;
