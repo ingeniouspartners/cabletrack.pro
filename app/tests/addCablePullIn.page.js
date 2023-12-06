f42d476281d763f5efb649584651169563cd0335
Andrea Jans <57602919+awjans@users.noreply.github.com>
2023-12-05 18:01:47 -1000

    more tests

--- app/tests/addCablePullIn.page.js
+++ app/tests/addCablePullIn.page.js
@@ -15,31 +15,6 @@ class AddCablePullInPage {
   async gotoListCablePullInPage(testController) {
     await testController.click(`#${NavListCablePullIn}`);
   }
-
-  async addPullIn(testController, pullIn) {
-    await testController.typeText('#project-form-name', project.name);
-    await testController.typeText('#project-form-code', project.code);
-    await testController.typeText('#project-form-contract', project.contract);
-    await testController.typeText('#project-form-bidNumber', project.bidNumber);
-    await testController.typeText('#project-form-jobPhone', project.jobPhone);
-    await testController.typeText('#project-form-jobFax', project.jobFax);
-    await testController.typeText('#project-form-jobEmail', project.jobEmail);
-    await testController.typeText('#project-form-notes', project.notes);
-    await testController.typeText('#project-form-mail-address', project.mailAddress.address);
-    await testController.typeText('#project-form-mail-address2', project.mailAddress.address2);
-    await testController.typeText('#project-form-mail-city', project.mailAddress.city);
-    await testController.typeText('#project-form-mail-state', project.mailAddress.state);
-    await testController.typeText('#project-form-mail-zip', project.mailAddress.zip);
-    // await testController.typeText('project-form-mailAddress-country', project.mailAddress.country);
-    await testController.typeText('#project-form-ship-address', project.shipAddress.address);
-    await testController.typeText('#project-form-ship-address2', project.shipAddress.address2);
-    await testController.typeText('#project-form-ship-city', project.shipAddress.city);
-    await testController.typeText('#project-form-ship-state', project.shipAddress.state);
-    await testController.typeText('#project-form-ship-zip', project.shipAddress.zip);
-    // await testController.typeText('project-form-shipAddress-country', project.shipAddress.country);
-    await testController.click('#project-form-submit input.btn.btn-primary');
-    await testController.click('button.swal-button--confirm');
-  }
 }
 
 export const addCablePullInPage = new AddCablePullInPage();
