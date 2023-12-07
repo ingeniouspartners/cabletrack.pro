2a074517513a65415e95237157fb5fa017b8264e
ula <unichols@hawaii.edu>
2023-12-07 12:14:24 -1000

    Something on company.testcafe.js got deleted  so this is putting it back

--- app/tests/company.testcafe.js
+++ app/tests/company.testcafe.js
@@ -14,11 +14,35 @@ const credentials2 = { username: 'ceo@foo.com', password: 'changeme' };
 fixture('company: cabletrack.pro localhost test with default db')
   .page('http://localhost:3000');
 
+const company =
+  {
+    name: 'A Test Company',
+    address: {
+      address: 'Address 123',
+      address2: 'Address 456',
+      city: 'The City',
+      state: 'NY',
+      zip: '12345',
+      country: 'US'
+    },
+    phone: '123-456-7890',
+    fax: '123-456-7890',
+    email: 'something@something.com',
+    logoURL: 'something.com',
+    _id: 'IDK123',
+  }
+
+const editcompany = {
+  name: 'Edit Company',
+};
+
 test('Test that Company List works', async (testController) => {
   await navBar.gotoSignInPage(testController);
   await signinPage.signin(testController, credentials1.username, credentials1.password);
   await navBar.gotoListCompanyPage(testController);
   await listCompanyPage.isDisplayed(testController);
+  await listCompanyPage.hasCompany(testController); // test if the company is added
+
 });
 
 test('Test that Company View works', async (testController) => {
@@ -26,6 +50,7 @@ test('Test that Company View works', async (testController) => {
   await signinPage.signin(testController, credentials2.username, credentials2.password);
   await navBar.gotoListCompanyPage(testController);
   await listCompanyPage.isDisplayed(testController);
+  await listCompanyPage.hasCompany(testController); // test if the company is added
   await listCompanyPage.gotoViewCompanyPage(testController);
   await viewCompanyPage.isDisplayed(testController);
 });
@@ -37,6 +62,8 @@ test('Test that Company Add works', async (testController) => {
   await listCompanyPage.isDisplayed(testController);
   await listCompanyPage.gotoAddCompanyPage(testController);
   await addCompanyPage.isDisplayed(testController);
+  await addCompanyPage.addCompany(testController, company); // test add project
+
 });
 
 test('Test that Company Edit works', async (testController) => {
@@ -44,6 +71,11 @@ test('Test that Company Edit works', async (testController) => {
   await signinPage.signin(testController, credentials2.username, credentials2.password);
   await navBar.gotoListCompanyPage(testController);
   await listCompanyPage.isDisplayed(testController);
+  await listCompanyPage.hasCompany(testController); // test if the company is added
   await listCompanyPage.gotoEditCompanyPage(testController);
   await editCompanyPage.isDisplayed(testController);
+  await editCompanyPage.editCompany(testController, editcompany);
+  //await navBar.gotoCompaniesPage(testController); Confused because of Companies versus company list
+  await listCompanyPage.gotoViewCompanyPage(testController);
+  await viewCompanyPage.checkView(testController, editcompany);
 });
--- app/tests/listCompany.page.js
+++ app/tests/listCompany.page.js
@@ -23,6 +23,10 @@ class ListCompanyPage {
   async gotoEditCompanyPage(testController) {
     await testController.click(`#${NavEditCompany}`);
   }
+  async hasCompany(testController) {
+    const companyCount = Selector('tr').count;
+    await testController.expect(companyCount).gte(3);
+  }
 }
 
 export const listCompanyPage = new ListCompanyPage();
